41cb0d93d6b169fbb1edba07e5893014562a5b1b
johannessnildal <johannes.snildal@gmail.com>
2024-04-30 00:03:46 +0200

    smooth hover effects

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -10,7 +10,7 @@ const Home = () => {
     <section className="flex size-full flex-col gap-5 text-white">
       <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
-          <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
+          <h2 className="glassmorphism2 max-w-[273px] rounded py-2 text-center text-base font-normal">
             No meetings scheduled
           </h2>
           <div className="flex flex-col gap-2">
