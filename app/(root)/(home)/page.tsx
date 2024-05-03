4e27a952f3f3b0148e37a64bd5ad1c5af97fa2cd
Johannes Snildal <johannessnildal@gmail.com>
2024-05-03 16:36:23 +0200

    made hero section dynamic to display user's first name or a standard message if not signed in

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import MeetingTypeList from '@/components/MeetingTypeList';
+import { useUser } from '@clerk/nextjs';
 
 const Home = () => {
   const now = new Date();
@@ -7,12 +8,14 @@ const Home = () => {
   const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
   const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })).format(now);
 
+  const { user } = useUser();
+
   return (
     <section className="flex size-full flex-col gap-5 text-white">
       <div className="h-[300px] justify-center items-center w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
           <h2 className="glassmorphism2 max-w-[273px] rounded py-2 text-center text-base font-normal">
-            Hello, welcome to Yoom!
+            Welcome {`${user?.firstName}!` || 'to Yoom!'}
           </h2>
           <div className="flex flex-col gap-2">
             <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
