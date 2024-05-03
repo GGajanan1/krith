c7923b6e2ddd9930ba35d0f571871e351d16076f
Johannes Snildal <johannessnildal@gmail.com>
2024-05-03 17:56:05 +0200

    minor design changes

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -12,7 +12,7 @@ const Sidebar = () => {
   const pathname = usePathname();
 
   return (
-    <section className='sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
+    <section className='sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-2 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
       <div className='flex flex-1 flex-col gap-6'>
         {sidebarLinks.map((link) => {
           const isActive = pathname === link.route || pathname.startsWith(`${link.route}/`);
@@ -23,7 +23,7 @@ const Sidebar = () => {
               key={link.label}
               className={cn('flex gap-4 items-center p-4 rounded-lg justify-start duration-300 ease-in-out', {
               'bg-blue-1': isActive,
-              'hover:bg-dark-2': !isActive,
+              'hover:bg-dark-1': !isActive,
               })}
             >
               <Image 
