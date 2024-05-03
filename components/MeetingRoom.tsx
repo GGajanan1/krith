18a06784f316fa0351d2a0bb96b6b1b26c57304b
Johannes Snildal <johannessnildal@gmail.com>
2024-05-03 17:04:58 +0200

    fixed bug that caused meeting room leave button to not route correctly

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -11,7 +11,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { LayoutList, Users } from 'lucide-react'
-import { useSearchParams } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import EndCallButton from './EndCallButton'
 import Loader from './Loader'
 import { Metadata } from 'next'
@@ -36,6 +36,8 @@ const MeetingRoom = () => {
   
   const { useCallCallingState } = useCallStateHooks();
   const callingState = useCallCallingState();
+
+  const router = useRouter();
 
   if(callingState !== CallingState.JOINED) return <Loader />
 
@@ -62,7 +64,7 @@ const MeetingRoom = () => {
       </div>
 
       <div className='fixed bottom-0 flex w-full items-center justify-center gap-5 flex-wrap p-2'>
-        <CallControls />
+        <CallControls onLeave={() => router.push('/')} />
 
         <DropdownMenu>
 
